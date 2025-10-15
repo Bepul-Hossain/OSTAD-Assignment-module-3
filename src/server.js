919d929db6eb6b98117bd0ef94f9125d4ade399a
bepul-bysl <bepul.hossain@byslglobal.com>
2025-10-15 17:07:40 +0600

    hello

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ let server;
 
 if (require.main === module) {
   // If the file is run directly, start the server
-  const PORT = process.env.PORT || 3001;
+  const PORT = 3001;
   server = app.listen(PORT, '0.0.0.0', () => console.log(`Server running at http://0.0.0.0:${PORT})`));
 }
 
